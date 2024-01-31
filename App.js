bc299f22289304db5bc5676d53edca641b2bd554
vasylku <vasylkuvayskov@gmail.com>
2024-01-30 23:08:09 -0600

    added forms info, adjusted colors, work on navigation native bar

--- App.js
+++ App.js
@@ -1,23 +1,41 @@
 import { StatusBar } from "expo-status-bar";
+import * as NavigationBar from "expo-navigation-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
 import IconButton from "./UI/IconButton";
+import { Colors } from "./constants/colors";
+import { useEffect } from "react";
 
 export default function App() {
 	const Stack = createNativeStackNavigator();
+	useEffect(() => {
+		async function getNavGone() {
+			await NavigationBar.setBackgroundColorAsync("transparent");
+		}
+
+		getNavGone();
+	}, []);
 
 	return (
 		<>
 			<StatusBar style="dark" />
 			<NavigationContainer>
-				<Stack.Navigator>
+				<Stack.Navigator
+					screenOptions={{
+						headerStyle: { backgroundColor: Colors.primary500 },
+						headerTintColor: Colors.gray700,
+						contentStyle: { backgroundColor: Colors.gray700 },
+					}}
+				>
 					<Stack.Screen
 						name="AllPlaces"
 						component={AllPlaces}
 						options={({ navigation }) => ({
+							title: "Favorite Places",
+
 							headerRight: ({ tintColor }) => (
 								<IconButton
 									icon="add"
@@ -28,7 +46,13 @@ export default function App() {
 							),
 						})}
 					/>
-					<Stack.Screen name="AddPlace" component={AddPlace} />
+					<Stack.Screen
+						name="AddPlace"
+						component={AddPlace}
+						options={{
+							title: "Add Place",
+						}}
+					/>
 				</Stack.Navigator>
 			</NavigationContainer>
 		</>
